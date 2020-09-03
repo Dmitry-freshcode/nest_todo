c9e477e2f0255ce81bf58c5895065e9af70bf14d
Dante1999033 <wlad.scheludko@gmail.com>
2020-09-03 15:07:19 +0300

    working server

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
       }
       async login(user: any) {        
         //console.log(user);
-        const payload = { username: user.username, sub: user.userId };
+        const payload = { username: user.username };
         return {
           access_token: this.jwtService.sign(payload, {secret : process.env.JWT_SECRET
           }),
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -11,17 +11,23 @@ import {
 import { TodoService } from './todo.service';
 
 import { CreateTodoDto } from './dto/creat-todo.dto';
+
+
+
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+//import { AuthService } from './auth/auth.service';
 //import { Todo } from './schemas/todo.schemas';
 
 @Controller()
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}  
-
+  @UseGuards(JwtAuthGuard)
   @Post('/todo')
   async create(@Body() createTodoDto: CreateTodoDto) {
     return await this.todoService.create(createTodoDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get('/todo')
   async findAll(@Body() body, @Query() query ){
 
@@ -35,7 +41,7 @@ export class TodoController {
       }      
     }
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete('/todo')
   async delete(@Body() body) {
     if (body._id) {
@@ -44,7 +50,7 @@ export class TodoController {
       return await this.todoService.deleteAll();
     }
   }
-
+  @UseGuards(JwtAuthGuard)
   @Patch('/todo')
   async update(@Body() body) {
     return await this.todoService.update(body._id);
