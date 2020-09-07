91513ecc1cb7879b8ede23923cd3897ebfcce083
Dante1999033 <wlad.scheludko@gmail.com>
2020-09-07 11:12:14 +0300

    add error response v.2

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,9 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from "passport-jwt";
-//import { ConfigService } from "@nestjs/config";
-//import { IUser } from "../users/interfaces/user.interface";
-//import { TokenService } from "src/token/token.service";
+
 
 
 @Injectable()
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel, InjectConnection } from '@nestjs/mongoose';
+import { Injectable ,HttpException,HttpStatus} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateTodoDto } from './dto/creat-todo.dto';
 import { Todo } from './schemas/todo.schemas';
@@ -9,40 +9,70 @@ export class TodoService {
   constructor(@InjectModel('Todo') private readonly todoModel: Model<Todo>) {}
 
   async create(createTodo: CreateTodoDto): Promise<Todo> {
-    const newTodo = new this.todoModel(createTodo);
+    try{
+      const newTodo = new this.todoModel(createTodo);
     return await newTodo.save();
+    }catch{
+      throw new HttpException('BAD_REQUEST : todo.create', HttpStatus.BAD_REQUEST);
+    }
+    
   }
 
   async findId(id: string): Promise<Todo[]> {
-    return await this.todoModel.find({ _id: id });
+    try{
+      return await this.todoModel.find({ _id: id });
+    } catch{
+      throw new HttpException('BAD_REQUEST : todo.findId', HttpStatus.BAD_REQUEST);
+    }
+    
   }
 
   async findAll(query): Promise<Todo[]> {
-    if (query.page > 0) {
-      return await this.todoModel
-        .find()
-        .skip(Number((query.page - 1) * query.perPage))
-        .limit(Number(query.perPage))
-        .exec();
+    try{
+      if (query.page > 0) {
+        return await this.todoModel
+          .find()
+          .skip(Number((query.page - 1) * query.perPage))
+          .limit(Number(query.perPage))
+          .exec();
+      }
+      return await this.todoModel.find().exec();
+    } catch{
+      throw new HttpException('BAD_REQUEST : todo.findAll', HttpStatus.BAD_REQUEST);
     }
-    return await this.todoModel.find().exec();
-  }
+    }
+   
 
   async deleteAll(): Promise<Object> {
-    return await this.todoModel.deleteMany({}).exec();
+    try{
+      return await this.todoModel.deleteMany({}).exec();
+    } catch{
+      throw new HttpException('BAD_REQUEST : todo.deleteAll', HttpStatus.BAD_REQUEST);
+    }
+    
   }
   async delete(id: string): Promise<Object> {
-    return await this.todoModel.deleteOne({ _id: id });
+    try{
+      return await this.todoModel.deleteOne({ _id: id });
+    } catch{
+      throw new HttpException('BAD_REQUEST : todo.delete', HttpStatus.BAD_REQUEST);
+    }
+    
   }
   async update(id: string): Promise<Object> {
-    if (await this.todoModel.findById(id).exec()) {
-      const [{ state: oldState }] = await this.todoModel
-        .find({ _id: id })
-        .exec();
-      return await this.todoModel
-        .updateOne({ _id: id }, { state: !oldState })
-        .exec();
+      try{
+        if (await this.todoModel.findById(id).exec()) {
+          const [{ state: oldState }] = await this.todoModel
+            .find({ _id: id })
+            .exec();
+          return await this.todoModel
+            .updateOne({ _id: id }, { state: !oldState })
+            .exec();
+        }
+        return 'id is not find in DB';
+      }catch{
+        throw new HttpException('BAD_REQUEST : todo.update', HttpStatus.BAD_REQUEST);
+      }
     }
-    return 'id is not find in DB';
-  }
+  
 }
